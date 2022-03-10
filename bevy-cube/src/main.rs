2e6365c9e16438afbffb6343d0bd6913632018d5
Keith Sharp <kms@passback.co.uk>
2022-03-10 13:11:22 +0000

    Rendering a lovely pink cube.

--- bevy-cube/src/main.rs
+++ bevy-cube/src/main.rs
@@ -1,3 +1,41 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(WindowDescriptor {
+            title: "Bevy Cube Experiment".to_string(),
+            width: 1600.,
+            height: 1200.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    let mesh = meshes.add(Mesh::from(shape::Cube { size: 1. }));
+    let material = materials.add(StandardMaterial {
+        base_color: Color::PINK,
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(PbrBundle {
+        mesh: mesh,
+        material: material,
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2., 2.5, 5.)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(DirectionalLightBundle { ..Default::default() });
 }
