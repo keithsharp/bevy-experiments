ec5b825ef222125a31cf257c7c3e51a236076759
Keith Sharp <kms@passback.co.uk>
2022-03-10 11:59:37 +0000

    Uses entities, components, and systems, to say hello.

--- bevy-hello/src/main.rs
+++ bevy-hello/src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Keith Sharp".to_string()));
+    commands.spawn().insert(Person).insert(Name("Cart Anderson".to_string()));
+}
+
+fn hello_world() {
+    println!("Hello, World!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello, {}!", name.0)
+    }
 }
