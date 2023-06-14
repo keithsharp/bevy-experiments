ac29d3afc5821b40c5c87e9d62d8d1adf52cfa75
Keith Sharp <kms@passback.co.uk>
2023-06-14 10:28:06 +0100

    bevy-ws: client is now a Bevy app using bevy-tokio-tasks.

--- bevy-ws/src/client.rs
+++ bevy-ws/src/client.rs
@@ -1,34 +1,54 @@
-use tracing::info;
-use tracing::warn;
-use tracing::Level;
-use tracing_subscriber;
+use bevy::log::LogPlugin;
+use bevy::prelude::*;
 
+use bevy_tokio_tasks::{TaskContext, TokioTasksPlugin, TokioTasksRuntime};
+
+use tracing::Level;
 use websockets::{Frame, WebSocket};
 
 const SERVER: &str = "ws://127.0.0.1:3000/websocket";
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let subscriber = tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)?;
+fn main() -> anyhow::Result<()> {
+    App::new()
+        .add_plugins(MinimalPlugins)
+        .add_plugin(LogPlugin {
+            level: Level::DEBUG,
+            ..Default::default()
+        })
+        .add_plugin(TokioTasksPlugin::default())
+        .add_startup_system(setup_ws_system)
+        .run();
 
+    Ok(())
+}
+
+fn setup_ws_system(runtime: ResMut<TokioTasksRuntime>) {
+    runtime.spawn_background_task(ws_task);
+}
+
+async fn ws_task(_ctx: TaskContext) {
     info!("Connecting to server: {}", SERVER);
-    let mut ws = WebSocket::connect(SERVER).await?;
+    let mut ws = WebSocket::connect(SERVER)
+        .await
+        .expect("should always be able to connect to the server");
 
     info!("Sending a ping message");
-    ws.send(websockets::Frame::Ping { payload: None }).await?;
+    ws.send(websockets::Frame::Ping { payload: None })
+        .await
+        .expect("should always be able to send a message");
 
     info!("Waiting on a response: ");
-    let message = ws.receive().await?;
+    let message = ws
+        .receive()
+        .await
+        .expect("should always be able ro recieve a message");
     match message {
         Frame::Pong { .. } => info!("Got a pong message"),
         message => warn!("Got a non-Pong message: {:?}", message),
     }
 
     info!("Closing WebSocket");
-    ws.close(None).await?;
-
-    Ok(())
+    ws.close(None)
+        .await
+        .expect("should always be able to close the WebSocket");
 }
